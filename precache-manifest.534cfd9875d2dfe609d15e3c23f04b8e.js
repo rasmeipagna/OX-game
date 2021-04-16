26258a9150784a4fb87772fce6278a23103185ab
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-04-16 14:05:45 +0200

    Updates

---
+++ precache-manifest.534cfd9875d2dfe609d15e3c23f04b8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fe3b257e09b7497b2f630d51cc6fe9",
+    "url": "/ox-game/index.html"
+  },
+  {
+    "revision": "b0ee578148b3f33480d5",
+    "url": "/ox-game/static/css/main.628544f5.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/ox-game/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "b0ee578148b3f33480d5",
+    "url": "/ox-game/static/js/main.0b666767.chunk.js"
+  },
+  {
+    "revision": "3e159872383c39565ed1",
+    "url": "/ox-game/static/js/runtime~main.0e7993ab.js"
+  }
+]);
